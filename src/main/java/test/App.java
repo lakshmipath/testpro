307d433c3aa7143b05b20b498f08fd540645d158
lakshmipath <pathi.devops75@gmail.com>
2023-08-11 09:00:46 +0530

    added

---
+++ src/main/java/test/App.java
@@ -0,0 +1,18 @@
+package test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    int x=10;
+    int y=30;
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+
+
+System.out.println( "Hello World!" );System.out.println( "Hello World!" );System.out.println( "Hello World!" );System.out.println( "Hello World!" );System.out.println( "Hello World!" );System.out.println( "Hello World!" );System.out.println( "Hello World!" );System.out.println( "Hello World!" );System.out.println( "Hello World!" );System.out.println( "Hello World!" );System.out.println( "Hello World!" );System.out.println( "Hello World!" );System.out.println( "Hello World!" );System.out.println( "Hello World!" );System.out.println( "Hello World!" );System.out.println( "Hello World!" );System.out.println( "Hello World!" );System.out.println( "Hello World!" );System.out.println( "Hello World!" );System.out.println( "Hello World!" );System.out.println( "Hello World!" );System.out.println( "Hello World!" );System.out.println( "Hello World!" );System.out.println( "Hello World!" );System.out.println( "Hello World!" );System.out.println( "Hello World!" );System.out.println( "Hello World!" );
+    }
+}
